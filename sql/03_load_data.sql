fa1be4236145694904825d6dcf8a5e1c062b0c59
Samuel-Boye-Abroquah <abroquahsamuel@gmail.com>
2026-09-23 15:38:00 +0000

    Add SQL script to load data into dim_customers and fact_sales

---
+++ sql/03_load_data.sql
@@ -0,0 +1,16 @@
+TRUNCATE TABLE dim_customers;
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/dim_customers.csv'
+INTO TABLE dim_customers
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/fact_sales.csv'
+INTO TABLE fact_sales
+FIELDS TERMINATED BY ',' 
+       OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(order_number, product_key, customer_key, order_date, ship_date, due_date,
+ quantity, price, sales)
